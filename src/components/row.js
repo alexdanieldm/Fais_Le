77926f0fa05be80559a9b2b030f4a9a79f832460
alexdanieldm <31324674+alexdanieldm@users.noreply.github.com>
2021-05-24 13:42:52 -0400

    refactor: Update folder arquitecture
    old files are now on the deprecated folder, and new files on root folder
    
    *chore: update eslintrc, prettierrc and gitignore

---
+++ src/components/row.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {StyleSheet, View} from 'react-native';
+
+const Footer = (  ) => {
+  return (
+    <View/>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
+
+export default Footer;
