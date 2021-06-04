17f0a8049dfe7cd75c3078cc526f14b3e1a78d9a
alexdanieldm <31324674+alexdanieldm@users.noreply.github.com>
2021-06-04 17:41:42 -0400

    chore: add screens folder
    chore: create logIn.js and signUp.js files

---
+++ src/screens/logIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const logIn = () => {
+	return (
+		<View>
+			<Text>logIn</Text>
+		</View>
+	);
+};
+
+export default logIn;
