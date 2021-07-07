1215af6d81e2538c8a808c40ba9e9caf0a9902a4
alexdanieldm <31324674+alexdanieldm@users.noreply.github.com>
2021-07-07 14:45:34 -0400

    test: if userMenu renders correctly
    build: add userMenu snapshot

--- __tests__/userMenu-test.js
+++ __tests__/userMenu-test.js
@@ -4,11 +4,12 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import Menu from '../src/screens/userMenu';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('Renders correctly', () => {
+	const tree = renderer.create(<Menu />).toJSON();
+	expect(tree).toMatchSnapshot();
 });
