73172ec772ffbe807175f9d5e070e0f268e4c495
alexdanieldm <31324674+alexdanieldm@users.noreply.github.com>
2021-07-12 10:14:17 -0400

    test(userMenu): Button navigate to ToDo screen
    build(npm): install and configure testing-library

--- __tests__/userMenu-test.js
+++ __tests__/userMenu-test.js
@@ -4,12 +4,28 @@
 
 import 'react-native';
 import React from 'react';
-import Menu from '../src/screens/userMenu';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react-native';
 
-it('Renders correctly', () => {
-	const tree = renderer.create(<Menu />).toJSON();
-	expect(tree).toMatchSnapshot();
+import Menu from '../src/screens/userMenu';
+import onSignOut from '../src/utils/onSignOut';
+
+describe('Screen', () => {
+	test('Should render correctly', () => {
+		const menuJSON = renderer.create(<Menu />).toJSON();
+		expect(menuJSON).toMatchSnapshot();
+	});
+});
+
+describe('Go Home Button', () => {
+	test('Should navigate to ToDo screen', async () => {
+		const goBack = jest.fn();
+		const { findByText } = render(<Menu navigation={{ goBack }} />);
+		const GoHomeButton = await findByText('Go Home');
+
+		fireEvent(GoHomeButton, 'press');
+		expect(goBack).toHaveBeenCalled();
+	});
 });
