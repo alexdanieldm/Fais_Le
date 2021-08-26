d94f64afb63263236299c0586477a12920ab0746
alexdanieldm <31324674+alexdanieldm@users.noreply.github.com>
2021-08-26 18:33:49 -0400

    test: Integrate Firebase Emulation Suit
    build(npm): add typehead jest plugin

--- __tests__/todo-test.js
+++ __tests__/todo-test.js
@@ -1,14 +1,38 @@
 import 'react-native';
 
+import { firebase as mockFirebase } from '../src/firebase/config';
+import { build, fake, sequence } from '@jackfranklin/test-data-bot';
+
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 
-import Todo from '../src/screens/userMenu';
+import Todo from '../src/screens/todo';
 
-jest.mock('');
+mockFirebase.firestore().useEmulator('localhost', 8080);
 
-afterEach(() => {
-  jest.clearAllMocks();
+const dataBuilder = build('Data', {
+  fields: {
+    uid: fake((f) => f.internet.password()),
+    fullName: fake((f) => f.name.findName()),
+    email: fake((f) => f.internet.email()),
+  },
 });
 
-test('test in progress', () => {});
+const data = dataBuilder();
+beforeAll(() => {
+  mockFirebase
+    .firestore()
+    .collection('users')
+    .doc(data.uid)
+    .set({
+      ...data,
+      joined: mockFirebase.firestore.FieldValue.serverTimestamp(),
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+});
+
+test('Add Item', () => {
+  expect(1).toBe(1);
+});
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  // setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)',
@@ -20,4 +21,8 @@ module.exports = {
       lines: 45,
     },
   },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 };
