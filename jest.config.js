24acb39438e73cf209636f58cee2fb497d9f3caf
alexdanieldm <31324674+alexdanieldm@users.noreply.github.com>
2021-09-09 18:17:20 -0400

    build(jest): to use jest-expo preset

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  preset: 'react-native',
+  preset: 'jest-expo',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)',
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
   ],
   watchPathIgnorePatterns: ['<rootDir>/node_modules'],
 
